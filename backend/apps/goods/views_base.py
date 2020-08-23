ae100167a8c02df372b1ad44594bd0b290a19d08
ScCcWe <1229659207@qq.com>
2020-08-24 00:47:25 +0800

    django自带的serializer

--- backend/apps/goods/views_base.py
+++ backend/apps/goods/views_base.py
@@ -3,7 +3,6 @@
 # file_name: views_base.py
 # author: ScCcWe
 # time: 2020/8/23 23:36
-from django.forms import model_to_dict
 from django.views.generic.base import View
 
 from goods.models import Goods
@@ -11,13 +10,11 @@ from goods.models import Goods
 
 class GoodsListView(View):
     def get(self, request):
-        json_list = []
         goods = Goods.objects.all()
-        for good in goods:
-            json_dict = model_to_dict(good)
-            json_list.append(json_dict)
 
-        from django.http import HttpResponse
+        from django.http import JsonResponse
+        from django.core import serializers
         import json
-        # 返回json，一定要指定类型content_type='application/json'
-        return HttpResponse(json.dumps(json_list, indent=4), content_type="application/json")
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+        return JsonResponse(json_data, safe=False)
