85cb1a99c057001496b36cb4a9b8fdd533fe1332
ScCcWe <1229659207@qq.com>
2020-08-24 00:43:04 +0800

    使用model_to_dict

--- backend/apps/goods/views_base.py
+++ backend/apps/goods/views_base.py
@@ -3,7 +3,7 @@
 # file_name: views_base.py
 # author: ScCcWe
 # time: 2020/8/23 23:36
-
+from django.forms import model_to_dict
 from django.views.generic.base import View
 
 from goods.models import Goods
@@ -14,12 +14,10 @@ class GoodsListView(View):
         json_list = []
         goods = Goods.objects.all()
         for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            # json_dict['category'] = good.category
-            json_dict['market_price'] = good.market_price
+            json_dict = model_to_dict(good)
             json_list.append(json_dict)
 
         from django.http import HttpResponse
         import json
+        # 返回json，一定要指定类型content_type='application/json'
         return HttpResponse(json.dumps(json_list, indent=4), content_type="application/json")
