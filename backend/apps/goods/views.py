b2dd7c91c840edbafa46d21de24ee0cd355dca94
ScCcWe <1229659207@qq.com>
2020-08-24 19:13:09 +0800

    使用generics.ListAPIView来进一步的简化代码

--- backend/apps/goods/views.py
+++ backend/apps/goods/views.py
@@ -1,22 +1,13 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-# mixins和generic一起使用
-# generics.GenericAPIView 继承APIView，封装了许多方法，比APIView功能更加强大
-# 用的时候需要定义queryset和serializer_class
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from .models import Goods
 from .serializers import GoodsSerializer
 
 
-# ListModelMixin里面list方法帮我们做好了分页和序列化的工作
-class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
+# 使用generics.ListAPIView来进一步的简化代码
+class GoodsListView(generics.ListAPIView):
     """
     商品列表页
     """
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
